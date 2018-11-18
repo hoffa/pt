ec34b1044aad11ebefc0da2a16515762c4621c29
Christoffer Rehn <crehn@outlook.com>
2018-11-18 16:57:57 +0900

    First commit

---
+++ pt.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
+
+	"gopkg.in/russross/blackfriday.v2"
+)
+
+type Post struct {
+	Path    string
+	Title   string
+	Content string
+}
+
+type Posts []Post
+
+func replaceExtension(path, ext string) string {
+	return path[0:len(path)-len(filepath.Ext(path))] + ext
+}
+
+func main() {
+	files, err := filepath.Glob("posts/*.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+	templ, err := template.ParseGlob("*.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var posts Posts
+	for _, path := range files {
+		target := replaceExtension(path, ".html")
+		log.Println(target)
+		f, err := os.Create(target)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		post := Post{
+			Path:    target,
+			Title:   string(b[2:bytes.IndexByte(b, byte('\n'))]),
+			Content: string(blackfriday.Run(b)),
+		}
+		posts = append(posts, post)
+		templ.ExecuteTemplate(f, "post.tmpl", post)
+	}
+	f, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	templ.ExecuteTemplate(f, "index.tmpl", posts)
+}
