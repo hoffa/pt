0df152a418a4b17eb2cdd93befb77f76c140eea1
Christoffer Rehn <crehn@outlook.com>
2018-12-02 11:48:25 +0900

    Overall improvements

--- pt.go
+++ pt.go
@@ -14,8 +14,9 @@ import (
 )
 
 type FrontMatter struct {
-	Title string `yaml:"title"`
-	Date  string `yaml:"date"`
+	Title  string `yaml:"title"`
+	Date   string `yaml:"date"`
+	Author string `yaml:"author"`
 }
 
 type Post struct {
@@ -28,47 +29,50 @@ func replaceExtension(path, ext string) string {
 	return path[0:len(path)-len(filepath.Ext(path))] + ext
 }
 
+func executeTemplate(source, target string, data interface{}) error {
+	log.Println(target)
+	f, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	tmpl, err := template.ParseFiles("layout.tmpl", source)
+	if err != nil {
+		return err
+	}
+	return tmpl.ExecuteTemplate(f, "layout", data)
+}
+
+func separateFrontMatter(b []byte) (fm, md []byte) {
+	i := bytes.Index(b[3:], []byte("---")) + 3
+	return b[3:i], b[i+3:]
+}
+
 func main() {
-	files, err := filepath.Glob("posts/*.md")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl, err := template.ParseGlob("*.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
 	var posts []Post
-	for _, path := range files {
-		target := replaceExtension(path, ".html")
-		log.Println(target)
-		f, err := os.Create(target)
-		if err != nil {
-			log.Fatal(err)
+	filepath.Walk(".", func(path string, f os.FileInfo, err error) error {
+		if filepath.Ext(path) != ".md" {
+			return nil
 		}
-		defer f.Close()
+		target := replaceExtension(path, ".html")
 		b, err := ioutil.ReadFile(path)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-		i := bytes.Index(b[3:], []byte("---")) + 3
+		fm, md := separateFrontMatter(b)
 		var frontMatter FrontMatter
-		if err := yaml.Unmarshal(b[3:i], &frontMatter); err != nil {
-			log.Fatal(err)
+		if err := yaml.Unmarshal(fm, &frontMatter); err != nil {
+			return err
 		}
+		content := string(blackfriday.Run(md))
 		post := Post{
 			FrontMatter: frontMatter,
 			Path:        target,
-			Content:     string(blackfriday.Run(b[i+3:])),
+			Content:     content,
 		}
 		posts = append(posts, post)
-		tmpl.ExecuteTemplate(f, "post.tmpl", post)
-	}
-	log.Println("index.html")
-	f, err := os.Create("index.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
+		return executeTemplate("post.tmpl", target, post)
+	})
 	sort.Slice(posts, func(i, j int) bool { return posts[i].Date > posts[j].Date })
-	tmpl.ExecuteTemplate(f, "index.tmpl", posts)
+	executeTemplate("index.tmpl", "index.html", posts)
 }
