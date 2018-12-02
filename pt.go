62f6e34113b568d5f8f815e391536dfa77a589da
Christoffer Rehn <crehn@outlook.com>
2018-12-02 14:47:12 +0900

    Update

--- pt.go
+++ pt.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,9 +15,9 @@ import (
 )
 
 type FrontMatter struct {
-	Title  string `yaml:"title"`
-	Date   string `yaml:"date"`
-	Author string `yaml:"author"`
+	Title string `yaml:"title"`
+	Date  string `yaml:"date"`
+	Hide  bool   `yaml:"hide"`
 }
 
 type Post struct {
@@ -30,7 +31,6 @@ func replaceExtension(path, ext string) string {
 }
 
 func executeTemplate(source, target string, data interface{}) error {
-	log.Println(target)
 	f, err := os.Create(target)
 	if err != nil {
 		return err
@@ -43,23 +43,31 @@ func executeTemplate(source, target string, data interface{}) error {
 	return tmpl.ExecuteTemplate(f, "layout", data)
 }
 
-func separateFrontMatter(b []byte) (fm, md []byte) {
-	i := bytes.Index(b[3:], []byte("---")) + 3
-	return b[3:i], b[i+3:]
+func separateFrontMatter(b []byte) ([]byte, []byte, error) {
+	i := bytes.Index(b[3:], []byte("---"))
+	if i == -1 {
+		return nil, nil, errors.New("missing front matter")
+	}
+	return b[3 : i+3], b[i+6:], nil
 }
 
 func main() {
 	var posts []Post
-	filepath.Walk(".", func(path string, f os.FileInfo, err error) error {
+	if err := filepath.Walk(".", func(path string, f os.FileInfo, err error) error {
 		if filepath.Ext(path) != ".md" {
 			return nil
 		}
+		log.Println(path)
 		target := replaceExtension(path, ".html")
 		b, err := ioutil.ReadFile(path)
 		if err != nil {
 			return err
 		}
-		fm, md := separateFrontMatter(b)
+		fm, md, err := separateFrontMatter(b)
+		if err != nil {
+			log.Print(err)
+			return nil
+		}
 		var frontMatter FrontMatter
 		if err := yaml.Unmarshal(fm, &frontMatter); err != nil {
 			return err
@@ -72,7 +80,10 @@ func main() {
 		}
 		posts = append(posts, post)
 		return executeTemplate("post.tmpl", target, post)
-	})
+	}); err != nil {
+		log.Fatal(err)
+	}
 	sort.Slice(posts, func(i, j int) bool { return posts[i].Date > posts[j].Date })
 	executeTemplate("index.tmpl", "index.html", posts)
 }
+
