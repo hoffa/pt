bceb1d6caad987fbcd9c0e484ed1b17e51686708
Christoffer Rehn <crehn@outlook.com>
2019-02-15 22:03:42 +0900

    Overhaul to simplify

--- pt.go
+++ pt.go
@@ -4,15 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"net/url"
 	"os"
 	"path/filepath"
-	"sort"
 	"text/template"
-	"time"
 
 	"github.com/BurntSushi/toml"
-	"github.com/gorilla/feeds"
 	"github.com/russross/blackfriday"
 )
 
@@ -23,17 +19,20 @@ type Config struct {
 }
 
 type FrontMatter struct {
-	Title string
-	Date  string
+	Title       string
+	Description string
+	Date        string
 }
 
+// Also store a list of all posts??
 type Post struct {
-	Title   string
-	Date    time.Time
-	URL     *url.URL
-	Path    string
-	Content string
-	Format  func(t time.Time) string
+	Title       string
+	Description string
+	Date        string
+	Path        string
+	Content     string
+	Index       bool
+	Posts       []Post
 }
 
 func replaceExtension(path, ext string) string {
@@ -62,41 +61,9 @@ func separateFrontMatter(b []byte) ([]byte, []byte) {
 	return b[3 : i+3], b[i+6:]
 }
 
-func writeRSS(config *Config, posts []Post) error {
-	author := &feeds.Author{Name: config.Author}
-	feed := &feeds.Feed{
-		Title:  config.Author,
-		Link:   &feeds.Link{Href: config.BaseURL},
-		Author: author,
-	}
-	var items []*feeds.Item
-	for _, post := range posts {
-		if post.Title == "" {
-			continue
-		}
-		items = append(items, &feeds.Item{
-			Title:   post.Title,
-			Author:  author,
-			Link:    &feeds.Link{Href: post.URL.String()},
-			Created: post.Date,
-			Content: post.Content,
-		})
-	}
-	feed.Items = items
-	f, err := os.Create("feed.xml")
-	if err != nil {
-		return err
-	}
-	return feed.WriteRss(f)
-}
-
 func main() {
 	var config Config
 	_, err := toml.DecodeFile("pt.toml", &config)
-	if err != nil {
-		panic(err)
-	}
-	base, err := url.Parse(config.BaseURL)
 	if err != nil {
 		panic(err)
 	}
@@ -105,50 +72,34 @@ func main() {
 		if filepath.Ext(path) != ".md" {
 			return nil
 		}
-		fmt.Println(path)
 		target := replaceExtension(path, ".html")
-		u, err := url.Parse(target)
-		if err != nil {
-			return err
-		}
 		b, err := ioutil.ReadFile(path)
 		if err != nil {
 			return err
 		}
 		var frontMatter FrontMatter
-		var date time.Time
 		fm, md := separateFrontMatter(b)
-		if fm != nil {
-			if err := toml.Unmarshal(fm, &frontMatter); err != nil {
-				return err
-			}
-			date, err = time.Parse(config.DateFormat, frontMatter.Date)
-			if err != nil {
-				return err
-			}
-		} else {
-			fmt.Println("note: missing front matter")
+		if err := toml.Unmarshal(fm, &frontMatter); err != nil {
+			return err
 		}
 		content := string(blackfriday.MarkdownCommon(md))
+		fmt.Println(path, frontMatter)
 		// &Post ?
 		post := Post{
 			Title:   frontMatter.Title,
-			Date:    date,
-			URL:     base.ResolveReference(u),
+			Date:    frontMatter.Date,
 			Path:    target,
 			Content: content,
-			Format: func(t time.Time) string {
-				return t.Format(config.DateFormat)
-			},
+			Index:   true,
 		}
 		posts = append(posts, post)
-		return executeTemplate("post.tmpl", target, post)
+		return executeTemplate("layout.tmpl", target, post)
 	}); err != nil {
 		panic(err)
 	}
-	sort.Slice(posts, func(i, j int) bool { return posts[i].Date.After(posts[j].Date) })
-	executeTemplate("index.tmpl", "index.html", posts)
-	if err := writeRSS(&config, posts); err != nil {
-		panic(err)
+	for _, post := range posts {
+		post.Posts = posts
+		executeTemplate("layout.tmpl", post.Path, post)
 	}
+	//executeTemplate("index.tmpl", "index.html", posts)
 }
