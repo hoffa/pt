70b6aee2afc592260de59c4e081901b13fb2f35b
Chris Rehn <crehn@outlook.com>
2021-10-30 14:02:16 -0700

    Fix stdin handling

--- pt.go
+++ pt.go
@@ -100,29 +100,12 @@ func separateContent(b []byte) ([]byte, []byte) {
 	return b[3 : i+3], b[i+6:]
 }
 
-func isTTY(f *os.File) bool {
-	stat, err := f.Stat()
-	check(err)
-	return stat.Mode()&os.ModeCharDevice != 0
-}
-
-// also exclude feed if stdin, since only one will be written to stdout
-// and title should be empty, seeing /dev/stdin is a bit of a hack
-
-func parsePage(f *os.File, baseURL, style string) *Page {
-	b, err := io.ReadAll(f)
-	check(err)
+func parsePage(b []byte, target, baseURL, style string) *Page {
 	fm, md := separateContent(b)
 	frontMatter := &FrontMatter{
 		Date: time.Now(),
 	}
 	check(yaml.Unmarshal(fm, frontMatter))
-	target := ""
-	if isTTY(f) {
-		target = replaceExtension(f.Name(), ".html")
-	} else {
-		frontMatter.Exclude = true
-	}
 	var enabledExtensions blackfriday.Extensions = blackfriday.CommonExtensions | blackfriday.Footnotes
 	var content []byte
 	if style == "" {
@@ -139,23 +122,25 @@ func parsePage(f *os.File, baseURL, style string) *Page {
 }
 
 func writePage(tmpl htmlTemplate.Template, page *Page) {
-	f := os.Stdout
-	if page.Path != "" {
+	if page.Path == "" {
+		check(tmpl.Execute(os.Stdout, page))
+	} else {
 		f, err := os.Create(page.Path)
 		check(err)
 		defer f.Close()
+		check(tmpl.Execute(f, page))
 	}
-	check(tmpl.Execute(f, page))
 }
 
 func writeRSS(tmpl textTemplate.Template, page *Page) {
-	f := os.Stdout
-	if page.Path != "" {
+	if page.Path == "" {
+		check(tmpl.Execute(os.Stdout, page))
+	} else {
 		f, err := os.Create(page.Path)
 		check(err)
 		defer f.Close()
+		check(tmpl.Execute(f, page))
 	}
-	check(tmpl.Execute(f, page))
 }
 
 func urlJoin(base, p string) string {
@@ -177,17 +162,22 @@ func main() {
 	var excluded []*Page
 	ps := flag.Args()
 	if len(ps) == 0 {
-		ps = []string{os.Stdin.Name()}
-	}
-	for _, p := range ps {
-		f, err := os.Open(p)
+		b, err := io.ReadAll(os.Stdin)
 		check(err)
-		defer f.Close()
-		page := parsePage(f, *baseURL, *style)
-		if page.Exclude {
-			excluded = append(excluded, page)
-		} else {
-			included = append(included, page)
+		page := parsePage(b, "", *baseURL, *style)
+		page.FrontMatter.Exclude = true
+		excluded = append(excluded, page)
+	} else {
+		for _, p := range ps {
+			b, err := os.ReadFile(p)
+			check(err)
+			target := replaceExtension(p, ".html")
+			page := parsePage(b, target, *baseURL, *style)
+			if page.Exclude {
+				excluded = append(excluded, page)
+			} else {
+				included = append(included, page)
+			}
 		}
 	}
 	sort.Slice(included, func(i, j int) bool { return included[i].Date.After(included[j].Date) })
